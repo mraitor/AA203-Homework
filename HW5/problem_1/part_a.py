922b0eb3adfd6db5d4f47d09aa0d57b9954c21f2
mraitor <michael.raitor@gmail.com>
2020-05-26 12:33:08 -0500

    problem 1a complete

--- HW5/problem_1/part_a.py
+++ HW5/problem_1/part_a.py
@@ -15,9 +15,46 @@ gamma = 0.95 # discount factor
 def Riccati(A,B,Q,R):
 
     # TODO implement infinite horizon riccati recursion
-    P = Q + np.dot(np.transpose(A),np.dot(P,A)) - np.dot(np.transpose(A), np.dot(P, np.dot(B, np.dot(np.linalg.inv(R + np.dot(np.transpose(B), np.dot(P,B))), np.dot(np.tranpsose(B),np.dot(P,A))))))
+    #matlab code that works
+    # P_current = Q;
+    # P_new = P_current;
+    # L_current = zeros(size(Q,1), size(R,2)); %general form, in ex. creates 4x1 zeros matrix
+    # L_new = L_current;
+    # firstIt = true; 
+    # while (norm(L_new - L_current, 2) >= 1e-4) || (firstIt == true)
+    #     if firstIt == true
+    #         firstIt = false;
+    #     end
+    #     L_current = L_new;
+    #     P_current = P_new;
+    #     L_new = -inv(R + B'*P_current*B)*(B'*P_current*A);
+    #     P_new = Q + L_new'*R*L_new + (A + B*L_new)'*P_current*(A + B*L_new);
+   
+    #                 end
+
+    #     P = P_new;
+    #     L = L_new;
+    #rewriting in stupid python
+    P_current = Q
+    P_new = P_current
+    #fix L dims
+    L_current = np.zeros([2,4])
+    L_new = L_current
+    firstIt = True
+    while (np.linalg.norm(L_new - L_current, 2) >= 1e-3) or firstIt:
+        if firstIt:
+            firstIt = False
+        L_current = L_new
+        P_current = P_new
+        L_new = np.linalg.inv(R + np.dot(np.transpose(B), np.dot(P_current, B)))@(np.dot(np.transpose(B),np.dot(P_current, A)))
+        P_new = Q + np.dot(np.transpose(L_new), np.dot(R,L_new)) + np.transpose(A + np.dot(B,L_new)) @ P_current @ (A+np.dot(B,L_new))
+        #print(L_new)
+    L = L_new
+    P = P_new   
+    #DARE:
+    # P = Q + np.dot(np.transpose(A),np.dot(P,A)) - np.dot(np.transpose(A), np.dot(P, np.dot(B, np.dot(np.linalg.inv(R + np.dot(np.transpose(B), np.dot(P,B))), np.dot(np.tranpsose(B),np.dot(P,A))))))
     #define K
-    K = np.dot(np.linalg.inv(R + np.dot(np.transpose(B), np.dot(P, B))), np.dot(np.transpose(B), np.dot(P,A)))
+    # K = np.dot(np.linalg.inv(R + np.dot(np.transpose(B), np.dot(P, B))), np.dot(np.transpose(B), np.dot(P,A)))
     return L,P
 
 
